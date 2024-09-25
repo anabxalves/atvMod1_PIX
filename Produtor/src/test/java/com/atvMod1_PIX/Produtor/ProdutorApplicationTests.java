b23b7d93ee2212cea23f88c22dbf88598431d2f5
anabxalves <anabxalves@gmail.com>
2024-09-25 10:56:00 -0300

    start projeto

---
+++ Produtor/src/test/java/com/atvMod1_PIX/Produtor/ProdutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atvMod1_PIX.Produtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
