1e5b357d7789f8fc749fac2ad5b3dd9ec21a4588
Caiobadv <caiobarretodv@hotmail.com>
2024-09-27 23:01:08 -0300

    adicionando menu de log aoauditor

--- Consumidor/src/main/java/com/atvMod1_PIX/Consumidor/ConsumidorApplication.py
+++ Consumidor/src/main/java/com/atvMod1_PIX/Consumidor/ConsumidorApplication.py
@@ -1,5 +1,6 @@
 import pika
 import json
+import os
 
 # Função para processar as mensagens recebidas
 def callback(ch, method, properties, body):
@@ -18,6 +19,22 @@ def write_to_log(message):
     with open(log_file_path, 'a') as log_file:
         log_file.write(message + '\n')
 
+# Função para ler e printar o arquivo de log
+def print_auditoria_log():
+    log_file_path = 'atvMod1_PIX/Consumidor/src/main/java/com/atvMod1_PIX/consumer_log.txt'
+    if os.path.exists(log_file_path):
+        with open(log_file_path, 'r') as log_file:
+            logs = log_file.readlines()
+            if logs:
+                print("### INÍCIO DO LOG DE AUDITORIA ###")
+                for log in logs:
+                    print(log.strip())
+                print("### FIM DO LOG DE AUDITORIA ###")
+            else:
+                print("O arquivo de log está vazio.")
+    else:
+        print("O arquivo de log não foi encontrado.")
+
 def main():
     url = 'amqps://vbiidqub:WB5wOr8lg7Aujc3ySCV0NdbLAWdIpcOC@prawn.rmq.cloudamqp.com/vbiidqub'
     connection_params = pika.URLParameters(url)
@@ -30,19 +47,20 @@ def main():
     # Configuração da fila e Routing Key com base na escolha do usuário
     queue_name, routing_keys = define_queue_and_routing_keys()
 
-    # Declarando a fila
-    channel.queue_declare(queue=queue_name)
+    if routing_keys:
+        # Declarando a fila
+        channel.queue_declare(queue=queue_name)
 
-    # Ligando a fila ao exchange com as chaves de roteamento calculadas
-    for routing_key in routing_keys:
-        print(f"Ligando a fila {queue_name} ao exchange com a chave de roteamento: {routing_key}")
-        channel.queue_bind(exchange='topic-exchange', queue=queue_name, routing_key=routing_key)
+        # Ligando a fila ao exchange com as chaves de roteamento calculadas
+        for routing_key in routing_keys:
+            print(f"Ligando a fila {queue_name} ao exchange com a chave de roteamento: {routing_key}")
+            channel.queue_bind(exchange='topic-exchange', queue=queue_name, routing_key=routing_key)
 
-    # Consumindo as mensagens da fila
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+        # Consumindo as mensagens da fila
+        channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
-    print(f" [*] Aguardando mensagens na fila {queue_name}. Para sair, pressione CTRL+C")
-    channel.start_consuming()
+        print(f" [*] Aguardando mensagens na fila {queue_name}. Para sair, pressione CTRL+C")
+        channel.start_consuming()
 
 def define_queue_and_routing_keys():
     routing_keys = []
@@ -50,17 +68,28 @@ def define_queue_and_routing_keys():
 
     # Menu para escolha
     print("Escolha sua função:")
-    print("1. Auditoria BACEN (Receber todas as mensagens)")
+    print("1. Auditoria BACEN")
     print("2. Banco (Receber mensagens apenas de bancos específicos)")
     print("3. Destinatário (Receber mensagens apenas para um destinatário específico)")
 
     escolha = input("Digite o número da sua escolha: ").strip()
 
     if escolha == '1':
-        # Auditoria BACEN: Receber todas as mensagens em uma fila geral
-        print("Escutando todas as mensagens PIX.")
-        queue_name = 'auditoria_bacen'
-        routing_keys.append('pix.#')
+        # Submenu da Auditoria BACEN
+        print("Escolha a opção de auditoria:")
+        print("1. Printar log de auditoria")
+        print("2. Receber todas as mensagens")
+
+        auditoria_escolha = input("Digite o número da sua escolha: ").strip()
+
+        if auditoria_escolha == '1':
+            print_auditoria_log()
+            return "", []  # Não precisa configurar fila para apenas printar o log
+        
+        elif auditoria_escolha == '2':
+            print("Escutando todas as mensagens PIX.")
+            queue_name = 'auditoria_bacen'
+            routing_keys.append('pix.#')
 
     elif escolha == '2':
         # Banco: Receber mensagens de bancos específicos (independentemente do destinatário)
